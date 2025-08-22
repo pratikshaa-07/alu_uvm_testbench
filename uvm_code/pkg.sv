262639ac0edbd99fc0870911520e33f46514f718
pratikshaa-07 <pratikshashetty125@gmail.com>
2025-08-22 10:51:04 +0530

    Create pkg.sv

---
+++ uvm_code/pkg.sv
@@ -0,0 +1,14 @@
+package pkg;
+`include "uvm_pkg.sv"
+`include "uvm_macros.svh"
+`include "item.sv"
+`include "seq.sv"
+`include "seqr.sv"
+`include "drv.sv"
+`include "mon.sv"
+`include "agt.sv"
+`include "sb.sv"
+`include "covg.sv"
+`include "env.sv"
+`include "tb.sv"
+endpackage
