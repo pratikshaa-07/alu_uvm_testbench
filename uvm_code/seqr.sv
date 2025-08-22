39d87c503977eb5a6a5b917aaaa9d97a1eb84b55
pratikshaa-07 <pratikshashetty125@gmail.com>
2025-08-22 10:45:25 +0530

    Create seqr.sv

---
+++ uvm_code/seqr.sv
@@ -0,0 +1,7 @@
+class seqr extends uvm_sequencer#(sitem);
+`uvm_component_utils(seqr)
+
+function new(string name="seqr",uvm_component parent=null);
+    super.new(name,parent);
+endfunction
+endclass
