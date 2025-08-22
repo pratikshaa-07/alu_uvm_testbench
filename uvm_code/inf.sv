8e50c21e4d50085eb52d0d7e5fd30fa747850e5a
pratikshaa-07 <pratikshashetty125@gmail.com>
2025-08-22 11:10:02 +0530

    Update inf.sv

--- uvm_code/inf.sv
+++ uvm_code/inf.sv
@@ -33,17 +33,18 @@ interface inf(input logic clk, rst);
   modport REF (clocking rcb);
     
 // property p2;
-//   @(posedge CLK);
-//  (INP_VALID==1) |-> !$isunknown(OPA);
-
+    //   @(posedge clk);
+    //  (inp_valud==1) |-> !$isunknown(opa);
+// endproperty
 //    assert property (p2)
 //       $info("INP_VALID is 1 and OPA is not unknown Assertion-2  Passed");
 //    else
 //       $error("INP_VALID is 1 and OPA is not unknow Assertion-2 Failed");
 
 //   property p3;
-//   @(posedge CLK);
-//   (INP_VALID==2) |-> !$isunknown(OPB);
+    //   @(posedge clk);
+    //   (inp_valid==2) |-> !$isunknown(opb);
+//   endproperty
 
 //    assert property (p3)
 //       $info("INP_VALID is 2 and OPB is not unknown Assertion-3  Passed");
@@ -51,17 +52,18 @@ interface inf(input logic clk, rst);
 //       $error("INP_VALID is 2 and OPB is not unknow Assertion-3 Failed");
 
 //   property p4;
-//   @(posedge CLK);
-//    (INP_VALID==3) |-> (!$isunknown(OPA) && !$isunknown(OPB));
-
-//    assert property (p3)
+    //   @(posedge clk);
+    //    (inp_valid==3) |-> (!$isunknown(opa) && !$isunknown(opb));
+//    endproperty
+//    assert property (p4)
 //       $info("INP_VALID is 3 and OPA and OPB is not unknown Assertion-4  Passed");
 //    else
 //       $error("INP_VALID is 3 and OPA and OPB is not unknow Assertion-4 Failed");
 
 //   property p5;
-//   @(posedge CLK);
-//    RST |-> (RES=='0 && E=0 && L=0 && G=0 && OFLOW=0 && COUT=0);
+    //   @(posedge clk);
+    //    rst |-> (res=='0 && e=0 && l=0 && g=0 && oflow=0 && cout=0);
+//    endproperty
 
 //    assert property (p5)
 //       $info("RST condition Assertion-5  Passed");
@@ -69,22 +71,33 @@ interface inf(input logic clk, rst);
 //       $error("RST condition Assertion-5 Failed");
 
 //   property p6;
-//   @(posedge CLK);
-//    CMD inside {14,15} |-> (RES='z);
-
+    //   @(posedge clk);
+    //    cmd inside {14,15} |-> (res='z);
+//    endproperty
 //    assert property (p6)
 //       $info("Invalid CMD for both modes Assertion-6  Passed");
 //    else
 //       $error("Inavlid CMD for both modes Assertion-6 Failed");
 
 //   property p7;
-//   @(posedge CLK);
-//    (INP_VALID==3) |-> !$isunknown(CMD);
+    //   @(posedge clk);
+//    (inp_valid==3) |-> !$isunknown(cmd);
+//    endproperty
 
 //    assert property (p7)
 //   $info("INP_VALID is 3 and CMD is not unknown Assertion-7  Passed");
 //    else
 //       $error("INP_VALID is 3 and CMD is not unknow Assertion-7 Failed");
+
+//   property p8;
+//   @(posedge clk);
+    //    (mode==1 && cmd inside {0,1,2,3,9,10} && inp_valid!=2'b11)|->##[1:16]inp_valid==2'b11;
+//   endproperty
+
+//    assert property (p8)
+//       $info("INP_VALID is 3 and OPA and OPB is not unknown Assertion-4  Passed");
+//    else
+//       $error("INP_VALID is 3 and OPA and OPB is not unknow Assertion-4 Failed");
 endinterface
     
 
