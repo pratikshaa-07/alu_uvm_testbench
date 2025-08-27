b7c6b98a954f32ba19b7e1694673eb7bdd0e4fa1
pratikshaa-07 <pratikshashetty125@gmail.com>
2025-08-27 14:09:58 +0530

    Update mon.sv

--- uvm_code/mon.sv
+++ uvm_code/mon.sv
@@ -24,18 +24,22 @@ endfunction
 //run phase
 virtual task run_phase(uvm_phase phase);
 super.run_phase(phase);
-i=0;
+i=0; 
 repeat (3) @(posedge vif.mcb);
 forever
 begin
-    i++;
+    i++; //increments for each transcation
+    
+    //16 clk cycle mechanism
+
+    //checking for invalid input_valid for both modes
     if ((vif.ce && (vif.inp_valid != 0 && vif.inp_valid != 3) && vif.mode && (vif.cmd inside {0,1,2,3,9,10})) ||
         (vif.ce && (vif.inp_valid != 0 && vif.inp_valid != 3) && (vif.mode == 0) && (vif.cmd inside {0,1,2,3,4,12,13})))
     begin
       flag = 0;
       repeat (16) @(posedge vif.mcb)
       begin
-       // `uvm_info("MONITOR", $sformatf("[MONITOR-%0d] inside 16 clk", i), UVM_LOW)
+
       if(vif.inp_valid==3)
       begin
           flag=1;
@@ -44,14 +48,20 @@ begin
       end
       end
     end
+
+    //for valid inp_valid 
     else
         flag=1;
+    //for multiplication
     if(flag && vif.mode && vif.cmd inside {9,10})
         repeat(2)@(vif.mcb);
     else
         repeat(1)@(vif.mcb);
 
+    //creating a new item  for each transaction
     mitem=sitem::type_id::create("mitem",this);
+    
+    //capturing the output of DUT
     mitem.opa=vif.opa;
     mitem.opb=vif.opb;
     mitem.cin=vif.cin;
@@ -67,9 +77,10 @@ begin
     mitem.err=vif.mcb.err;
     mitem.oflow=vif.mcb.oflow;
     mitem.cout=vif.mcb.cout;
-  `uvm_info("MONITOR",$sformatf("[MONITOR-%0d in] ce=%0d | mode=%0d | cmd=%0d | |inp_valid=%0d | opa=%0d | opb=%0d | cin=%0d ",i,vif.ce,vif.mode,vif.cmd,vif.inp_valid,vif.opa,vif.opb,vif.cin),UVM_LOW);
-    //mport.write(mitem);
-  `uvm_info("MONITOR",$sformatf("[MONITOR-%0d out] res=%0d | cout=%0d | oflow=%0d | e=%0d | g=%0d | l=%0d | err=%0d |",i,mitem.res,mitem.cout,mitem.oflow,mitem.e,mitem.g,mitem.l,mitem.err),UVM_LOW);
+  `uvm_info("MONITOR",$sformatf("[MONITOR-%0d in] ce=%0d | mode=%0d | cmd=%0d | |inp_valid=%0d | opa=%0d | opb=%0d | cin=%0d ",i,vif.ce,vif.mode,vif.cmd,vif.inp_valid,vif.opa,vif.opb,vif.cin),UVM_LOW)
+  `uvm_info("MONITOR",$sformatf("[MONITOR-%0d out] res=%0d | cout=%0d | oflow=%0d | e=%0d | g=%0d | l=%0d | err=%0d |",i,mitem.res,mitem.cout,mitem.oflow,mitem.e,mitem.g,mitem.l,mitem.err),UVM_LOW)
+
+  //cloning the captured data and then writing into fifo for safety
   $cast(temp,mitem.clone());
   mport.write(temp);
   repeat(1)@(vif.mcb);
